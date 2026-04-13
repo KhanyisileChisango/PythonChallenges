92755cb02680d546749fd8ff3b656d54f83879d8
KhanyisileChisango <khanyisile.chisango@gmail.com>
2026-04-13 19:26:39 +0200

    13/04/2026 Challenge

---
+++ MadLibs.py
@@ -0,0 +1,8 @@
+adjective1 = input("Adjective:")
+adjective2 = input("Adjective:")
+famousperson = input("Famous person:")
+
+madlib = f"Coding is so much {adjective1}. Learning how to do it is {adjective2}. \
+In 5 years I hope to be half as successful as {famousperson}" 
+
+print(madlib)
